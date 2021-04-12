0b8a026b2f769835b0ba0fa35740e6c3d3484717
Sayali Karnewar <33855068+sayalikarnewar@users.noreply.github.com>
2021-04-13 00:39:13 +0530

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from script import get_word, search
 
 def routes(app):
-    app.router.add_get('/', get_word)
-    app.router.add_post('/', search, name='search')
+    app.router.add_get('https://sayalikarnewar.github.io/dictionary/', get_word)
+    app.router.add_post('https://sayalikarnewar.github.io/dictionary/', search, name='search')
